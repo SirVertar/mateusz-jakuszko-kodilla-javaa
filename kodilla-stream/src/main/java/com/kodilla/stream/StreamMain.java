b44423dcbf87dcf120afda524e58e2014c9f869b
SirVertar <mateusz.jakuszko@gmail.com>
2019-11-12 18:48:27 +0100

    Task 7.2

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,20 +1,28 @@
 package com.kodilla.stream;
 
-import com.kodilla.stream.beautifier.PoemBeautifier;
-import com.kodilla.stream.lambda.ExpressionExecutor;
-import com.kodilla.stream.reference.FunctionalCalculator;
+import com.kodilla.stream.book.Book;
+import com.kodilla.stream.book.BookDirectory;
+import com.kodilla.stream.iterate.NumbersGenerator;
+
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class StreamMain {
     public static void main(String[] args) {
+        BookDirectory theBookDirectory = new BookDirectory();
 
-        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        Map<String, Book> theResultMapOfBooks = theBookDirectory.getList().stream()
+                .filter(book -> book.getYearOfPublication() > 2005)
+                .collect(Collectors.toMap(Book::getSignature, book -> book));
 
-        System.out.println(poemBeautifier.beautify("to LoWer Case Test", String::toLowerCase));
-        System.out.println(poemBeautifier.beautify("to upper case test", text -> text.toUpperCase()));
-        System.out.println(poemBeautifier.beautify("text cut", text -> text.substring(1,5)));
-        System.out.println(poemBeautifier
-                .beautify("Replace all a wit b hahaha", text -> text.replace("a","b")));
+        System.out.println("# elements: " + theResultMapOfBooks.size());
+        theResultMapOfBooks.entrySet().stream()
+                .map(entry -> entry.getKey() + ": " + entry.getValue())
+                .forEach(System.out::println);
 
-
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
+
+
 }
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/immutable/ForumUser.java
@@ -0,0 +1,11 @@
+package com.kodilla.stream.immutable;
+
+public final class ForumUser {
+    private final String username;
+    private final String realName;
+
+    public ForumUser(final String username, final String realName) {
+        this.username = username;
+        this.realName = realName;
+    }
+}
