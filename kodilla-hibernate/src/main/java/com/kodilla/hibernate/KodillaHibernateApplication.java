98b4bdce4aac8f0916655acf0e28bec43384c079
SirVertar <mateusz.jakuszko@gmail.com>
2020-01-28 21:54:18 +0100

    Task 17.1_v1

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/KodillaHibernateApplication.java
@@ -1,4 +1,12 @@
 package com.kodilla.hibernate;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class KodillaHibernateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KodillaHibernateApplication.class, args);
+    }
 }
