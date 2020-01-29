bbf74cb1d437546414df1addf04ddcc9bfd97747
SirVertar <mateusz.jakuszko@gmail.com>
2020-01-29 20:33:06 +0100

    Task 17.2_v1

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/TaskList.java
@@ -1,4 +1,52 @@
 package com.kodilla.hibernate.tasklist;
 
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "TASKLISTS")
+@Access(AccessType.PROPERTY)
 public class TaskList {
+    private int id;
+    private String listName;
+    private String description;
+
+    public TaskList(){
+
+    }
+
+    public TaskList(String listName, String description) {
+        this.listName = listName;
+        this.description = description;
+    }
+
+    @Id
+    @GeneratedValue
+    @NotNull
+    @Column(name = "ID", unique = true)
+    public int getId() {
+        return id;
+    }
+
+    @Column(name = "LIST_NAME")
+    public String getListName() {
+        return listName;
+    }
+
+    @Column(name = "DESCRIPTION")
+    public String getDescription() {
+        return description;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setListName(String lastName) {
+        this.listName = lastName;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }
--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -1,4 +1,35 @@
 package com.kodilla.hibernate.tasklist.dao;
 
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.util.List;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TaskListDaoTestSuite {
+    @Autowired
+    private TaskListDao taskListDao;
+
+    @Test
+    public void testFindByListName() {
+        //Given
+        TaskList taskList = new TaskList("TO_DO_LIST", "THINGS TO DO");
+
+        //When
+        taskListDao.save(taskList);
+
+        //Then
+        String listName = taskList.getListName();
+        List<TaskList> loadedList = taskListDao.findByListName(listName);
+        Assert.assertEquals("TO_DO_LIST", loadedList.get(0).getListName());
+
+        //Clean up
+        taskListDao.deleteAll();
+    }
 }
