49d7689eb44a9b7afca3c3f37bc72b03426c4e3e
SirVertar <mateusz.jakuszko@gmail.com>
2020-05-12 21:11:47 +0200

    Task 31.2

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/invoice/simple/SimpleItem.java
@@ -0,0 +1,4 @@
+package com.kodilla.stream.invoice.simple;
+
+public class SimpleItem {
+}
---
+++ kodilla-testing2/src/main/java/com/kodilla/testing2/google/GoogleTestingApp.java
@@ -0,0 +1,8 @@
+package com.kodilla.testing2.google;
+
+public class GoogleTestingApp {
+    public static void main(String[] args) {
+        System.setProperty("webdriver.gecko.driver","c:\\Selenium-drivers\\Firefox\\geckodriver.exe");
+        System.setProperty("webdriver.gecko.driver","c:\\Selenium-drivers\\Chrome\\geckodriver.exe");
+    }
+}
